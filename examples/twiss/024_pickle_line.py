595aca15d1448a1ccab9ef88389f321b0582badd
giadarol <giovanni.iadarola@cern.ch>
2023-05-11 23:18:56 +0200

    Working on line pickling

---
+++ examples/twiss/024_pickle_line.py
@@ -0,0 +1,19 @@
+import time
+import multiprocessing as mp
+
+import numpy as np
+
+import xtrack as xt
+import xpart as xp
+
+from xobjects.context_cpu import XobjectPointer
+
+from cpymad.madx import Madx
+
+# Load the line
+line = xt.Line.from_json(
+    '../../test_data/hllhc15_noerrors_nobb/line_w_knobs_and_particle.json')
+#line.particle_ref = xp.Particles(p0c=7e12, mass=xp.PROTON_MASS_EV)
+
+import pickle
+ss = pickle.dumps(line)
