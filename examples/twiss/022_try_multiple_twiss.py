5c644dff61370ded59aeb685b6cdb5fcb75aee18
giadarol <giovanni.iadarola@cern.ch>
2023-05-11 09:22:47 +0200

    Explore multiprocessing

--- examples/twiss/022_try_multiple_twiss.py
+++ examples/twiss/022_try_multiple_twiss.py
@@ -3,6 +3,8 @@ import time
 import xtrack as xt
 import xpart as xp
 
+from xobjects.context_cpu import XobjectPointer
+
 from cpymad.madx import Madx
 
 # Load the line
@@ -44,10 +46,51 @@ for on_x1 in on_x1_values:
     line.vars['on_x1'] = on_x1
     buffers.append(line.tracker._buffer.buffer.copy())
 
-twisses = []
-for bb in buffers:
-    line._buffer.buffer = bb
-    twisses.append(line.twiss())
+# td = line.tracker._tracker_data
+# td._element_ref_data = XobjectPointer(td._element_ref_data)
+# td._element_dict = None
+# td._elements = None
+# td.element_classes = None
+# td._ElementRefClass = None
 
-for on_x1, tw in zip(on_x1_values, twisses):
-    print(f'on_x1 = {on_x1}, px["ip1"] = {tw["px", "ip1"]*1e6:f}e-6')
+input = {'buffer': None, 'tw_kwargs': {}}
+
+# def f_for_pool(input):
+#     buffer = input['buffer']
+#     tw_kwargs = input['tw_kwargs']
+
+#     line.tracker._buffer.buffer = buffer
+#     return line.twiss(**tw_kwargs)
+
+def f_for_pool(input):
+    return 2 * input
+
+inputs = []
+for buffer in buffers:
+    iii = input.copy()
+    iii['buffer'] = buffer
+    inputs.append(iii)
+
+# twisses = map(f_for_pool, inputs)
+if __name__ == '__main__':
+    # import multiprocessing as mp
+    # mp.set_start_method('spawn')
+    # pool = mp.Pool(processes=3)
+    # twisses = pool.map(f_for_pool, [1,2,3])
+    import multiprocessing as mp
+    # mp.set_start_method('spawn')
+    q = mp.Queue()
+    p = mp.Process(target=f_for_pool, args=(q,))
+    p.start()
+
+# pool = mp.Pool(processes=3)
+# twisses = pool.map(f_for_pool, [1,2,3])
+
+# twisses = []
+# for bb in buffers:
+#     line._buffer.buffer = bb
+#     twisses.append(line.twiss())
+
+# for on_x1, tw in zip(on_x1_values, twisses):
+#     print(f'on_x1 = {on_x1}, px["ip1"] = {tw["px", "ip1"]*1e6:f}e-6')
+
