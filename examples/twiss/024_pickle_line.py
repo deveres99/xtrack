2e63b6b758e850a03e881e5b4649237579d2ca5e
giadarol <giovanni.iadarola@cern.ch>
2023-05-11 23:41:22 +0200

    Pickled with var manager

--- examples/twiss/024_pickle_line.py
+++ examples/twiss/024_pickle_line.py
@@ -13,7 +13,7 @@ from cpymad.madx import Madx
 # Load the line
 line = xt.Line.from_json(
     '../../test_data/hllhc15_noerrors_nobb/line_w_knobs_and_particle.json')
-line._var_management = None
+# line._var_management = None
 #line.particle_ref = xp.Particles(p0c=7e12, mass=xp.PROTON_MASS_EV)
 
 import pickle
